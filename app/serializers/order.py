1bb582ac1d3d8463ec30d1902be88500449d4f2a
Ghalnas <frs.grv@gmail.com>
2017-03-15 19:02:12 +0100

    added validation to order serializer

--- app/serializers/order.py
+++ app/serializers/order.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from rest_framework import serializers
 from app.models.address import Address
 from app.models.vehicle import Vehicle
@@ -18,3 +20,15 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'gas_name', 'gas_price', 'gas_quantity', 'date_refill', 'date_created', 'is_payed', 'is_canceled',
             'address', 'vehicle', 'user')
+
+    def validate(self, data):
+        """
+        Ensure the passwords are the same
+        """
+        tomorrow = timezone.now() + datetime.timedelta(days=1)
+        tomorrow_to_midnight = tomorrow.replace(hour=0, minute=0, second=0, microsecond=0)
+        if data["date_refill"] <= tomorrow_to_midnight:
+            raise serializers.ValidationError(
+                "The refill date should be The day after tomorrow"
+            )
+        return data
